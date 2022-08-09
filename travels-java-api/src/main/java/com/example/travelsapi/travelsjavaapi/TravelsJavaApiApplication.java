4ae1ea59f221434a45427f6b8448d3a567fa0dfc
rtrem027 <renato.roriz@outlook.com>
2022-08-09 20:51:13 -0300

    init

---
+++ travels-java-api/src/main/java/com/example/travelsapi/travelsjavaapi/TravelsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelsapi.travelsjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsJavaApiApplication.class, args);
+	}
+
+}
---
+++ travels-java-api/src/test/java/com/example/travelsapi/travelsjavaapi/TravelsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelsapi.travelsjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
